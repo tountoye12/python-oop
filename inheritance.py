255314eab8843baaaaf4369533e0ebf2d8289153
Diallo <mamadouadama609@gmail.com>
2024-07-30 01:31:12 -0500

    MRO =====> Method Resolution Order

--- inheritance.py
+++ inheritance.py
@@ -7,7 +7,33 @@ class Bird(Animal):
     def fly(self):
         print("I am flying")
 
-b1 = Bird()
 
-b1.walk()
-b1.fly()
+# b1 = Bird()
+#
+# b1.walk()
+# b1.fly()
+
+class A:
+    def __init__(self):
+        print("In A")
+
+    def future_1(self):
+        print("Future 1 in A")
+
+
+class B:
+    def __init__(self):
+        print("In B")
+
+    def future_1(self):
+        print("Future 1 in B")
+
+
+# MRO ==> Method resolution order
+class C(B, A):
+    def __init__(self):
+        print("In C")
+        super().__init__()  # In C In B
+
+
+c1 = C()
